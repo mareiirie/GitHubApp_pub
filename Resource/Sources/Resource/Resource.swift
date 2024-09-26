38d565b05c5993eb49c7d55b05972f720df5fc3f
IrieMarei <marei_irie@openwork.co.jp>
2024-09-26 16:34:33 +0900

    packageのignoreも解除して復活

---
+++ Resource/Sources/Resource/Resource.swift
@@ -0,0 +1,16 @@
+//
+//  Resource.swift
+//  GithubApp
+//
+//  Created by marei_irie on 2024/09/12.
+//
+
+import Foundation
+
+public enum AccessToken: String {
+    case githubAPI = ""
+}
+
+public enum BaseURL: String {
+    case githubBaseURL = "https://api.github.com"
+}
